1b490a3c5cdbf329f7c26861b3031e1f738af565
shtanriverdi <shtanriverdi@yahoo.com>
2024-02-03 15:46:27 +0300

    Clean Up the Comment

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,11 @@
 // Fetch rockets data
 import { getRocketsData, getWeatherData } from "./utils/endpoints.js";
 
-// Represents a real time Socket.IO server
-import { Server } from "socket.io";
-// Represents Http library
-import { createServer } from 'node:http';
-
 // Initialize Express Backend
 import express from "express";
 // Express Application for routes, this is not a server!
 const port = 3000;
 const app = express();
-// Creates "server" for socket.io
-const server = createServer(app);
-// Server constructor
-const io = new Server(server);
-
-// // Socket.io
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
-// });
 
 app.get("/", (req, res) => {
   res.send("Home Page");
@@ -28,23 +14,21 @@ app.get("/", (req, res) => {
 // Rockets
 app.get("/getRocketsData", async (req, res) => {
   const rocketsData = await getRocketsData();
-  console.log("rocketsData: ", rocketsData, typeof rocketsData);
-  // for (const rocket in rocketsData) {
-  //   console.log(typeof rocket, rocket);
-  // }
+  console.log("Rockets data: ");
+  for (const rocket of rocketsData) {
+    console.log(rocket);
+  }
   res.json(rocketsData);
-  // res.send("Getting rockets data...");
 });
 
 // Weather
 app.get("/getWeatherData", async (req, res) => {
   const weatherData = await getWeatherData();
-  console.log("weatherData: ", weatherData, typeof weatherData);
-  // for (const rocket in rocketsData) {
-  //   console.log(typeof rocket, rocket);
-  // }
+  console.log("Weather data: ");
+  for (const weather of weatherData) {
+    console.log(weather);
+  }
   res.json(weatherData);
-  // res.send("Getting weather data...");
 });
 
 app.listen(port, () => {
