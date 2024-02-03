93b94b40e8f589c49b85ce10e17eb489aa99b6fc
shtanriverdi <shtanriverdi@yahoo.com>
2024-02-03 20:33:36 +0300

    Weather socket is working

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ import { Server } from "socket.io";
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: '*', // TODO
   },
 });
 
--- frontend/rocket-telemetry-system-frontend/src/Weather.jsx
+++ frontend/rocket-telemetry-system-frontend/src/Weather.jsx
@@ -7,7 +7,29 @@ const socket = io("http://localhost:3000/weather-telemetry");
 
 export default function Weather() {
   // Weather state
-  const [weatherData, setWeatherData] = useState(null);
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [weatherData, setWeatherData] = useState({
+    temperature: 44.49625110792446,
+    humidity: 0.37254237437334053,
+    pressure: 1063.8342725422417,
+    precipitation: {
+      probability: 0.14629280109282006,
+      rain: false,
+      snow: false,
+      sleet: false,
+      hail: false,
+    },
+    time: "2024-02-03T12:49:03.005984",
+    wind: {
+      direction: "SW",
+      angle: 113.98393803672059,
+      speed: 22.481814742673823,
+    },
+  });
+
+  const handleSocketConnection = (isConnected) => {
+    setIsConnected(isConnected);
+  };
 
   // Runs only at the beginning
   useEffect(() => {
@@ -21,6 +43,7 @@ export default function Weather() {
     socket.on("disconnect", () => {
       console.log("Socket disconnected");
       // Do stuff when disconnection happens
+      handleSocketConnection(false);
     });
 
     // Close the socket on unmount of component
@@ -32,8 +55,15 @@ export default function Weather() {
   return (
     <>
       <h1>Weather Status</h1>
-      <p>Wehather data: </p>
-      {weatherData}
+      <p>Socket: {isConnected}</p>
+      <p>Humidity: {weatherData.humidity}</p>
+      {/* <p>Precipitation: {weatherData.precipitation}</p> */}
+      <p>Pressure: {weatherData.pressure}</p>
+      <p>Temperature: {weatherData.temperature}</p>
+      <p>Time: {weatherData.time}</p>
+      {/* <p>Wind: {weatherData.wind}</p> */}
+      {/* <p>Angle: {weatherData.angle}</p> */}
+      {/* <p>Direction: {weatherData.direction}</p> */}
     </>
   );
 }
