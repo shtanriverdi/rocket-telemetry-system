e45dce126e579f30d2ce154236e8e951036824bc
shtanriverdi <shtanriverdi@yahoo.com>
2024-02-08 20:37:28 +0300

    Banner & Author

--- frontend/rocket-telemetry-system-frontend/src/App.jsx
+++ frontend/rocket-telemetry-system-frontend/src/App.jsx
@@ -5,12 +5,15 @@ import "./app.css";
 export default function App() {
   return (
     <>
-      <div className="flex-container">
-        <h1>Rocket Telemetry System</h1>
-        <small>&nbsp; version 0.1</small>
+      <div className="banner">
+        <h2>Rocket Telemetry System</h2>
+        <p>version 0.1</p>
       </div>
       <Weather />
       <Rockets />
+      <small className="m-bs">
+        Created by <b>Süha Tanrıverdi.</b> 2024
+      </small>
     </>
   );
 }
